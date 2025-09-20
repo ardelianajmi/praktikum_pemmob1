4b8aad7b443a748afa8ac8ba8fd95df2722b4359
ardelianajmi <najmiraissaadelia@gmail.com>
2025-09-20 20:36:14 +0700

    initial commit

---
+++ app/src/main/java/com/example/prakpemobpert1/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.prakpemobpert1
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.prakpemobpert1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        binding.btnToPage2.setOnClickListener {
+            startActivity(Intent(this, Halaman2Activity::class.java))
+        }
+    }
+}
