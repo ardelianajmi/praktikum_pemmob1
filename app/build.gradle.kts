d17ceb1108ac084094dfad7e456c045e7292adb5
ardelianajmi <najmiraissaadelia@gmail.com>
2025-09-22 21:16:17 +0700

    initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.prakpemobpert1"
+    namespace = "com.example.pemmobpertama"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.prakpemobpert1"
+        applicationId = "com.example.pemmobpertama"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
@@ -26,27 +26,27 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
 
-    buildFeatures {      // ✅ ganti pakai ini
+    buildFeatures {
         viewBinding = true
     }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/pemmobpertama/MainActivity.kt
+++ app/src/main/java/com/example/pemmobpertama/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.pemobpertama
+package com.example.pemmobpertama
 
 import android.content.Intent
 import android.os.Bundle
@@ -6,7 +6,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.pemobpertama.databinding.ActivityMainBinding
+import com.example.pemmobpertama.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
